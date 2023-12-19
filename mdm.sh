0940ba0c207beebafe9cc8a5cb0e11b73ec58726
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2023-12-19 17:05:31 -0500

    Removed File Clutter

---
+++ mdm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+RED='\033[0;31m'
+GRN='\033[0;32m'
+BLU='\033[0;34m'
+NC='\033[0m'
+echo ""
+echo -e "Auto Tools for MacOS"
+echo ""
+
+echo -e "${GRN}Bypass on Recovery"
+if [ -d "/Volumes/Macintosh HD - Data" ]; then
+    diskutil rename "Macintosh HD - Data" "Data"
+fi
+echo -e "${GRN}Entering user credentials"
+# echo -e "${BLU}Press Enter to move to the next step"
+realName="Mac"
+username="Mac"
+passw="1234"
+dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
+echo -e "${GREEN}Creating users"
+# Create user
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+mkdir "/Volumes/Data/Users/$username"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+echo -e "${GREEN}Successfully blocked host${NC}"
+# echo "Remove config profile"
+touch /Volumes/Data/private/var/db/.AppleSetupDone
+rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+echo "----------------------"
+rm ../../Volumes/Macintosh\ HD/var/db/.AppleSetupDone
