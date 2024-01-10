2535c833065a9d7fd5618667c5b4dd8c30ca8fe7
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2024-01-10 16:25:12 -0500

    Added All chap. 4 exercises

--- DataStructuresUsingC/ch04/acc_string.c
+++ DataStructuresUsingC/ch04/acc_string.c
@@ -105,3 +105,130 @@ int acc_strinsrt(char* text, char* ins_str, char* merge_str, int pos) {
 
     return 0; 
 }
+
+char* acc_delsub(char* text, const char*str) {
+    char* new_str = (char*)malloc(200 * sizeof(char));
+    if (new_str == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int i = 0, j = 0, found = 0, k, n = 0, copy_loop = 0;
+
+    while (text[i] != '\0') {
+        j = 0, found = 0, k = i;
+        while (text[k] == str[j] && str[j] != '\0') {
+            k++;
+            j++;
+        }
+        if (str[j] == '\0')
+            copy_loop = k;
+        new_str[n] = text[copy_loop];
+        i++;
+        copy_loop++;
+        n++;
+    }
+    new_str[n] = '\0';
+
+    return new_str;
+}
+
+char* acc_strrplc(char* str, const char* pat, const char* rep_pat) {
+    char* new_str = (char*)malloc(200 * sizeof(char));
+    if (new_str == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0, j = 0, k, n = 0, copy_loop = 0, rep_index = 0;
+    while (str[i] != '\0') {
+        j = 0, k = i;
+        while (str[k] == pat[j] && pat[j] != '\0') {
+            k++;
+            j++;
+        }
+        if (pat[j] == '\0') {
+            copy_loop = k;
+            while (rep_pat[rep_index] != '\0') {
+                new_str[n] = rep_pat[rep_index];
+                rep_index++;
+                n++;
+            }
+        }
+        new_str[n] = str[copy_loop];
+        i++;
+        copy_loop++;
+        n++;
+    }
+    new_str[n] = '\0';
+    return new_str;
+}
+
+char (*acc_shortstrs(char names[][10], int n))[10] {
+    char temp[10];
+    int i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - 1; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                strcpy(temp, names[j]);
+                strcpy(names[j], names[j + 1]);
+                strcpy(names[j + 1], temp);
+            }
+        }
+    }
+
+    char (*sorted_names)[10] = (char (*)[10])malloc(n * sizeof(names[0]));
+
+    if (sorted_names == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < n; i++) {
+        strcpy(sorted_names[i], names[i]);
+    }
+
+    return sorted_names;
+}
+
+void acc_count(const char *text, int *word_count, int *line_count, int *char_count) {
+    int i = 0;
+
+    printf("\n This is stuff %s", text);
+
+    while (text[i] != '\0') {
+        (*char_count)++;
+
+        if (text[i] == '\n' || i == 79)
+            (*line_count)++;
+
+        if (text[i] == ' ' && text[i + 1] != ' ')
+            (*word_count)++;
+
+        i++;
+    }
+}
+
+int is_palindrome(const char *str) {
+    int i = 0, j, length = 0;
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        length++;
+        i++;
+    }
+
+    i = 0;
+    j = length - 1;
+
+    while (i < length / 2) {
+        if (str[i] == str[j]) {
+            i++;
+            j--;
+        } else {
+            return 0;  // Not a palindrome
+        }
+    }
+
+    return 1;  // Palindrome
+}
--- DataStructuresUsingC/ch04/acc_string.h
+++ DataStructuresUsingC/ch04/acc_string.h
@@ -14,3 +14,13 @@ char* acc_strextr(const char*, int, int, int, int);
 char* acc_strrev(char*);
 
 int acc_strinsrt(char*, char*, char*, int);
+
+char* acc_delsub(char*, const char*);
+
+char* acc_strrplc(char*, const char*, const char*)
+
+char* acc_shortstrs(char, int);
+
+void acc_count(const char*, int*, int*, int*);
+
+int is_palindrome(const char*);
